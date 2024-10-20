9ed7c257f97d3a4ed5f626c0c7383b977ce05027
ostfriese4 <144824657+ostfriese4@users.noreply.github.com>
2024-10-20 18:59:26 +0200

    Create app_init.py

---
+++ app_init.py
@@ -0,0 +1,26 @@
+def ImportToKernel(k):
+  apps=[]
+  apps.append("system_search_gui")
+  apps.append("calculator_app")
+  apps.append("tictactoe_app")
+  apps.append("test_apps")
+  apps.append("settings_app")
+  apps.append("graphs_app")
+  apps.append("ships_app")
+  apps.append("clocks_app")
+  apps.append("apple_theme")
+  apps.append("propaganda_app")
+  apps.append("screensaver")
+  apps.append("extra_characters_gui")
+  apps.append("system_search_gui")
+
+  w=k.get("gui").window(title="starting")
+  l=k.get("widgets").Label(w,text="")
+  def code():
+    name=apps.pop()
+    l.set_text(name)
+    k.get(name)
+    if apps==[]:
+      w.close()
+      proc.delete()
+  proc=k.get("kernel").process(k,name="init",code=code)
